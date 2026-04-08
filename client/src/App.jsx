0d390d93b495fdd664ea3378770a867eb8049d44
niat-web <psm@nxtwave.in>
2026-04-08 17:35:55 +0530

    fourth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import PackageList from './components/PackageList';
 import Alert from './components/Alert';
 import './App.css';
 
-const API_BASE_URL = 'https://warehouse-33lv.onrender.com';
+const API_BASE_URL =   'http://localhost:5000/api/packages';
 
 function App() {
   const [packages, setPackages] = useState([]);
